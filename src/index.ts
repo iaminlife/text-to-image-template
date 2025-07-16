005f6a19867ce68daef670cd41c4be16c554c691
iaminlife <151754552+iaminlife@users.noreply.github.com>
2025-07-16 20:57:38 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk octopus",
+      prompt: "Bbikini girl",
     };
 
     const response = await env.AI.run(
