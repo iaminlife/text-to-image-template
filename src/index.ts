2ab32684922f4963dca9c81c574cc6d0eab3e758
iaminlife <151754552+iaminlife@users.noreply.github.com>
2025-07-16 21:20:46 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Whale+Breach+Moment,+Lenticular+Sunset,+Recollection,+broken+patterns,+Fogged+Film,+soft+bokeh",
+      prompt: "bikini+girl,+Beach+Moment,+Sunset,+realistic,+Film,+soft+bokeh",
     };
 
     const response = await env.AI.run(
